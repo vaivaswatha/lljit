f973a43ff45db5ebed1bc47b110c74934a3d27de
Vaivaswatha Nagaraj <vaivaswatha@zilliqa.com>
2020-03-18 21:22:10 +0530

    Add a testsuite to demo boost exception bug

--- Makefile
+++ Makefile
@@ -1,11 +1,16 @@
 CFLAGS=-I/usr/lib/llvm-9/include -std=c++14 -D_GNU_SOURCE -D__STDC_CONSTANT_MACROS -D__STDC_FORMAT_MACROS -D__STDC_LIMIT_MACROS -gdwarf-4 -Wall
-LDFLAGS=-L/usr/lib/llvm-9/lib -lLLVM-9
+LDFLAGS=-L/usr/lib/llvm-9/lib -lLLVM-9 -lboost_unit_test_framework
+
+all : lljit.exe testsuite.exe
 
 lljit.exe : JITD.h JITD.cpp main.cpp
 	${CXX} ${CFLAGS} JITD.cpp main.cpp -o lljit.exe ${LDFLAGS}
 
+testsuite.exe : JITD.h JITD.cpp testsuite.cpp
+	${CXX} ${CFLAGS} JITD.cpp testsuite.cpp -o testsuite.exe ${LDFLAGS}
+
 clean :
-	-rm lljit.exe cscope.out TAGS
+	-rm lljit.exe testsuite.exe cscope.out TAGS
 
 tags :
 	ctags -eR
---
+++ testsuite.cpp
@@ -0,0 +1,50 @@
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE LLTest
+#include <boost/test/unit_test.hpp>
+
+#include "JITD.h"
+
+// Compiles InputFilename and executes "void jitentry(void)" in it.
+void execLLFile(const std::string &InputFilename) {
+  using namespace ExampleVM;
+
+  ExampleJIT::init();
+  auto SJ = ExampleJIT::create(InputFilename);
+  if (!SJ) {
+    auto Err = SJ.takeError();
+    auto M = llvm::toString(std::move(Err));
+    BOOST_FAIL(M);
+  }
+  // Look for function named "jitentry" in LLVM-IR and run it.
+  auto ExampleMainAddr = (*SJ)->getAddressFor("jitentry");
+  if (!ExampleMainAddr) {
+    auto Err = SJ.takeError();
+    auto M = llvm::toString(std::move(Err));
+    BOOST_FAIL(M);
+  }
+  auto ExampleMain = reinterpret_cast<void (*)()>(*ExampleMainAddr);
+
+  ExampleMain();
+
+  // Without this dummy try-catch, test case "test_throw" will segfault.
+  try {
+    throw std::exception();
+  } catch (std::exception) {
+    ;
+  }
+}
+
+BOOST_AUTO_TEST_SUITE(lljit_testsuite)
+
+BOOST_AUTO_TEST_CASE( lljit_helloworld ) {
+  execLLFile("examples/helloworld.ll");
+}
+
+BOOST_AUTO_TEST_CASE( test_throw ) {
+  BOOST_CHECK_THROW(
+    throw std::exception(),
+    std::exception
+    );
+}
+
+BOOST_AUTO_TEST_SUITE_END()
